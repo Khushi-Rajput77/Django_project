1e66c905ffdb220114a4aad8797fe2a5c9332477
Khushi Thakur <khushithakur7676@gmail.com>
2026-09-20 18:50:49 +0530

    add

--- krishna_portal/settings.py
+++ krishna_portal/settings.py
@@ -15,10 +15,13 @@ load_dotenv(BASE_DIR / '.env')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-^ajwux_gq-@5550#=b5^o#p_$u_!7@-c+ozhe7e@@r5tj4jbou')
+from django.core.exceptions import ImproperlyConfigured
 
-DEBUG = os.getenv('DEBUG', 'True').lower() in ('true', '1', 't')
+SECRET_KEY = os.getenv('SECRET_KEY')
+if not SECRET_KEY:
+    raise ImproperlyConfigured("SECRET_KEY is missing or empty in .env")
 
+DEBUG = os.getenv('DEBUG', 'False').lower() in ('true', '1', 't')
 ALLOWED_HOSTS = [host.strip() for host in os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1,[::1],testserver').split(',') if host.strip()]
 
 # Application definition
